e7b57a05aa1243c30f0374b892080378b3f43523
Nagisa9029 <tzr.schicksal@gmail.com>
2020-06-10 17:22:29 +0900

    Add User_Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, uniqueness: true
-  validates :credit_card, presence: true, uniqueness: true
 end
