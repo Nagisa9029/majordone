f370a00ffe9fa6d01795aa16f3fe880d371ea99f
Nagisa9029 <tzr.schicksal@gmail.com>
2020-06-15 01:00:18 +0900

    Add root_Views

--- app/controllers/roots_controller.rb
+++ app/controllers/roots_controller.rb
@@ -6,41 +6,48 @@ class RootsController < ApplicationController
 
     # image2 = MiniMagick::Image.open('./app/assets/images/graph_0.png')
     # array2 = Numo::DFloat.cast(image2.get_pixels)
+  
     
 
-    def execute(target_file, top = 10)
-      data_array = calc_length(target_file)
-      data_array.sort_by { |b| b[:length] }[0..(top - 1)]
-    end
 
-    def calc_length(target_file)
-      ary = []
-      Dir.glob("./app/assets/images/*") do |item|
-        u_image = MiniMagick::Image.open(target_file)
-        u_numo_array = Numo::DFloat.cast(u_image.get_pixels)
-        u_filename = target_file
+    # def execute(target_file, top = 10)
+    #   data_array = calc_length(target_file)
+    #   data_array.sort_by { |b| b[:length] }[0..(top - 1)]
+    # end
 
-        w_image = MiniMagick::Image.open(item)
-        w_numo_array = Numo::DFloat.cast(w_image.get_pixels)
-        w_filename = item
+    # def calc_length(target_file)
+    #   ary = []
+    #   Dir.glob("./app/assets/images/*") do |item|
+    #     u_image = MiniMagick::Image.open(target_file)
+    #     u_numo_array = Numo::DFloat.cast(u_image.get_pixels)
+    #     u_filename = target_file
 
-        temp_data_params = { file: w_filename, length: length(u_numo_array, w_numo_array) }
-        ary << temp_data_params
-      end
-      ary
-    end
+    #     w_image = MiniMagick::Image.open(item)
+    #     w_numo_array = Numo::DFloat.cast(w_image.get_pixels)
+    #     w_filename = item
 
-    def length(array1, array2)
-      diff_abs(array1, array2).sum
-    end
+    #     temp_data_params = { file: w_filename, length: length(u_numo_array, w_numo_array) }
+    #     ary << temp_data_params
+    #   end
+    #   ary
+    # end
 
-    def diff_abs(array1, array2)
-      (array1 - array2).abs
-    end
+    # def length(array1, array2)
+    #   diff_abs(array1, array2).sum
+    # end
+
+    # def diff_abs(array1, array2)
+    #   (array1 - array2).abs
+    # end
   
 
-    result = execute('./app/assets/images/first.png', 10)
-    binding.pry
+    # result = execute('./app/assets/images/first.png', 10)
+    
+
+
+
+
+    
     # result.each do |r|
     #   p r
     # end
